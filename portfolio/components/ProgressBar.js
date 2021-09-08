dcf48000b2c66fcf4366b5d0e0471bf024ec9815
123MayankSharma <Mayanksharma922001@gmail.com>
2021-09-08 19:03:48 +0530

    added the skill section with changes accomodating dark theme

--- portfolio/components/ProgressBar.js
+++ portfolio/components/ProgressBar.js
@@ -4,7 +4,7 @@ const ProgressBar = ({ pb }) => {
 
             <div className="h-3 relative max-w-screen rounded-full overflow-hidden">
                 <div className="w-screen h-screen bg-gray-400 absolute"></div>
-                <div id='progress-bar' className="h-full w-full bg-red-500 absolute" style={{ width: `${pb}%` }}></div>
+                <div id='progress-bar' className="h-full w-full bg-indigo-500 dark:bg-red-500 absolute" style={{ width: `${pb}%` }}></div>
             </div>
             {/* <h1>{Math.ceil(pb)}% Done!</h1> */}
 
--- portfolio/components/skills/Skills.js
+++ portfolio/components/skills/Skills.js
@@ -10,7 +10,7 @@ const Skill = () => {
         <h1>
             Programming Languages
         </h1>
-        <div className="border-2 border-green-700 w-100 max-h-full rounded-md  sm:max-w-7xl">
+        <div className="border-2 border-gray-800 dark:border-green-700 mb-7  w-100 max-h-full rounded-md  sm:max-w-7xl">
 
             <span className="text-left ml-2">C++</span>
             <div className="ml-2 mr-2 mb-4"><ProgressBar pb={40} /></div>
@@ -20,6 +20,63 @@ const Skill = () => {
             <div className="ml-2 mr-2 mb-4"><ProgressBar pb={25} /></div>
             <span className="text-left ml-2">C</span>
             <div className="ml-2 mr-2 mb-4"><ProgressBar pb={25} /></div>
+
+        </div>
+
+        <h1>
+            Tools
+        </h1>
+        <div className="border-2 border-gray-800 dark:border-green-700 w-100 mb-7 max-h-full rounded-md  sm:max-w-7xl">
+
+            <span className="text-left ml-2">VS Code</span>
+            <div className="ml-2 mr-2 mb-4"><ProgressBar pb={40} /></div>
+            <span className="text-left ml-2">NeoVim</span>
+            <div className="ml-2 mr-2 mb-4"><ProgressBar pb={55} /></div>
+
+        </div>
+
+
+        <h1>Frameworks and Libraries</h1>
+        <div className="border-2 border-gray-800 dark:border-green-700 mb-7  w-100 max-h-full rounded-md  sm:max-w-7xl">
+
+            <span className="text-left ml-2">React.js</span>
+            <div className="ml-2 mr-2 mb-4"><ProgressBar pb={45} /></div>
+            <span className="text-left ml-2">Next.js</span>
+            <div className="ml-2 mr-2 mb-4"><ProgressBar pb={55} /></div>
+            <span className="text-left ml-2">Tailwind CSS</span>
+            <div className="ml-2 mr-2 mb-4"><ProgressBar pb={30} /></div>
+            <span className="text-left ml-2">Node.js</span>
+            <div className="ml-2 mr-2 mb-4"><ProgressBar pb={38} /></div>
+            <span className="text-left ml-2">Git and Github</span>
+            <div className="ml-2 mr-2 mb-4"><ProgressBar pb={40} /></div>
+
+
+        </div>
+
+
+        <h1>Operating System</h1>
+        <div className="border-2 border-gray-800 dark:border-green-700 mb-7  w-100 max-h-full rounded-md  sm:max-w-7xl">
+
+            <span className="text-left ml-2">Linux</span>
+            <div className="ml-2 mr-2 mb-4"><ProgressBar pb={50} /></div>
+            <span className="text-left ml-2">Windows</span>
+            <div className="ml-2 mr-2 mb-4"><ProgressBar pb={25} /></div>
+
+
+        </div>
+
+        <h1>Core Subjects</h1>
+        <div className="border-2 border-gray-800 dark:border-green-700 mb-7  w-100 max-h-full rounded-md  sm:max-w-7xl">
+
+            <span className="text-left ml-2">Operating System</span>
+            <div className="ml-2 mr-2 mb-4"><ProgressBar pb={45} /></div>
+            <span className="text-left ml-2">Networking</span>
+            <div className="ml-2 mr-2 mb-4"><ProgressBar pb={45} /></div>
+            <span className="text-left ml-2">SQL</span>
+            <div className="ml-2 mr-2 mb-4"><ProgressBar pb={60} /></div>
+            <span className="text-left ml-2">DBMS</span>
+            <div className="ml-2 mr-2 mb-4"><ProgressBar pb={40} /></div>
+
 
         </div>
 
